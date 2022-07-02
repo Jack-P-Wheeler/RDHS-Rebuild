427c635d2f4441a2a9c812cd0f1fa2431006a8e8
Jack Wheeler <jack.wheeler@rogers.com>
2022-07-02 15:22:06 -0400

    Finished up the mobile menu and added its javascript

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Script for the main page of Russell Gardeners
+var mobileMenuButton = document.getElementById("mobileMenuButton")
+var mobileMenu = document.getElementById("mobileMenuDropdown")
+var mobileMenuOpen = false
+
+mobileMenuButton.addEventListener("click", function(){
+    if(mobileMenuOpen){
+        mobileMenuOpen = false;
+        mobileMenu.style.display = "none";
+    }
+    else{
+        mobileMenuOpen = true;
+        mobileMenu.style.display = "flex";
+    }
+});
+    
