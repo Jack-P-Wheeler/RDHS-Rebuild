a9d1214d699039f7583e736b354567fff17d3573
Jack Wheeler <jackwheeler@Margarets-MacBook-Air.local>
2022-07-11 14:34:05 -0400

    Tweaked the javascipt for the mpbile menu

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //Script for the main page of Russell Gardeners
 var mobileMenuButton = document.getElementById("mobileMenuButton")
 var mobileMenu = document.getElementById("mobileMenuDropdown")
+var body = document.getElementsByTagName("body")[0]
 var mobileMenuOpen = false
 
-mobileMenuButton.addEventListener("click", function(){
-    if(mobileMenuOpen){
+body.addEventListener("click", function(event){
+    if (event.target == mobileMenuButton){
+        if(mobileMenuOpen){
+            mobileMenuOpen = false;
+            mobileMenu.style.display = "none";
+        }
+        else{
+            mobileMenuOpen = true;
+            mobileMenu.style.display = "flex";
+        }
+    }else{
         mobileMenuOpen = false;
         mobileMenu.style.display = "none";
     }
-    else{
-        mobileMenuOpen = true;
-        mobileMenu.style.display = "flex";
-    }
 });
-    
+
